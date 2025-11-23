530d0e7c9bc36500ad47137d15549eaf5cc784e3
Andrew David Kay <andrewdavidkay@icloud.com>
2025-11-22 20:28:35 -0500

    drizzle and better auth setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,13 @@ import { Link } from "@tanstack/react-router";
 export default function Header() {
 	return (
 		<header className="p-4 flex items-center bg-white">
-			<h1 className="ml-4 text-xl font-semibold">
-				<Link to="/">
-					<img src="/logo.svg" alt="level logo" className="h-10" />
-				</Link>
-			</h1>
+			<div className="w-full max-w-2xl mx-auto">
+				<h1 className="ml-4 text-xl font-semibold">
+					<Link to="/">
+						<img src="/logo.svg" alt="level logo" className="h-10" />
+					</Link>
+				</h1>
+			</div>
 		</header>
 	);
 }
